297b12227b7595995bc67409e0889158f2742a7d
eyalHadad <eyalhadad@outlook.com>
2020-11-16 11:50:56 +0200

    fixNegetiveValue

--- power.c
+++ power.c
@@ -4,15 +4,31 @@
 
 double Exponent(int x){
     double ret=1;
+    if(x>0){
     for(int i =0 ; i<x;i++){
         ret=ret*EXPON;
+    }
+    }
+    else 
+    {
+        for(int i =0 ; i>x;i--){
+        ret=1/(1/(ret)*EXPON);
+        }
     }
     return ret;
 }
 double power(double y,int x){
       double ret=1;
+    if(x>0){
     for(int i =0 ; i<x;i++){
         ret=ret*y;
+        }
+    }
+    else 
+    {
+        for(int i =0 ; i>x;i--){
+        ret=1/(1/(ret)*y);
+        }
     }
     return ret;
 }
